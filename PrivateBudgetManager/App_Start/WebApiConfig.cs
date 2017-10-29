6e9aae395d3bf2c6927c577f5ff4bc772b02dd0a
Jonas Lykke <jona711p@student.eadania.dk>
2017-10-29 17:53:28 +0100

    API Virker 50%
    Mangler bare Create og Update på transactioner
    Mangler også Create, Update og Delete på kategorier

--- PrivateBudgetManager/App_Start/WebApiConfig.cs
+++ PrivateBudgetManager/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace PrivateBudgetManager
 {
@@ -15,7 +12,13 @@ namespace PrivateBudgetManager
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
+                name: "TransactionsAPI",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "CategoriesAPI",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
---
+++ PrivateBudgetManager/Controllers/CategoriesAPIController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Mvc;
+using PrivateBudgetManager.Db;
+
+namespace PrivateBudgetManager.Controllers
+{
+    public class CategoriesAPIController : ApiController
+    {
+        TransactionsDb transactionsDb = new TransactionsDb();
+        Dictionary<int, string> tempDictionary = new Dictionary<int, string>();
+
+        // GET: api/CategoriesAPI
+        public Dictionary<int, string> Get()
+        {
+            List<SelectListItem> tempSelectListItem = transactionsDb.GetCategories();
+
+            tempDictionary.Clear();
+
+            foreach (SelectListItem item in tempSelectListItem)
+            {
+                tempDictionary.Add(int.Parse(item.Value), item.Text);
+            }
+
+            return tempDictionary;
+        }
+
+        // GET: api/CategoriesAPI/5
+        public Dictionary<int, string> Get(int id)
+        {
+            List<SelectListItem> tempSelectListItem = transactionsDb.GetCategories();
+
+            tempDictionary.Clear();
+
+            foreach (SelectListItem item in tempSelectListItem)
+            {
+                if (int.Parse(item.Value) == id)
+                {
+                    tempDictionary.Add(int.Parse(item.Value), item.Text);
+                }
+            }
+
+            return tempDictionary;
+        }
+
+        // POST: api/CategoriesAPI
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/CategoriesAPI/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/CategoriesAPI/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
